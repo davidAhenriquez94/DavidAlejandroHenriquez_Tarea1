05c54bf813a3476d6cc47b0f96957bcf2280a055
DavidAlejandroHenriquez <da.henriquez10@uniandes.edu.co>
2017-03-09 09:47:57 -0500

    2.0

--- leapfrog1.c
+++ leapfrog1.c
@@ -3,68 +3,53 @@
 #include <stdlib.h>
 
 void leapfrog(float *x, float *v, int i,int j, float h , float B);
-int trans(int i, int j);
 
 float dt = 0.005, B = 1.0;
-int N = 64, T = 1000,i,j;
+float F_actual,F_futuro,v_medio;
+int N = 64, T = 10000001,i,j,k,n,m;
 
 int main (int argc, char ** argv){
 
 //Creacion de variables posicion y velocidad
-	float *x = malloc(N*T*sizeof(float));
-	float *v = malloc(N*T*sizeof(float));
-
-//Asignacion de valores iniciales }
+	float *x = malloc(N*sizeof(float));
+	float *v = malloc(N*sizeof(float));
+	
+	x[0] = 0.0;
+	x[N-1] = 0.0;
+	v[0] = 0.0;
+	v[N-1] = 0.0;
+//Asignacion de valores iniciales 
 	
 	for(i = 1; i<N-1; i++){
-		x[trans(i,0)] = sin(M_PI*i/(N-1));
-	    v[trans(i,0)] = 0.0;
-	}
-
-//Asigna valor 0 a los extremos
-	for(j = 0; j<T; j++ ){
-    	x[trans(0,j)] = 0.0;
-		x[trans(N-1,j)] = 0.0;
-	    v[trans(0,j)] = 0.0;
-	    v[trans(N-1,j)] = 0.0;
+	  x[i] = sin(M_PI*i/(N-1));
+	  v[i] = 0.0; 
 	}
 
 //solucion de la ecuacion diferencial
+	float x_temp[N-1];
 	for(j = 0; j<T;j++){
-#pragma omp parallel for \shared(x,v),private(j)
-		for(i = 1; i<N-1; i++){
-			leapfrog(x,v,i,j,dt,B);
-		}
-	}
-  
-//imprime los valores de posicion
-	printf("posiciones \n");
-	for(i = 0; i<N; i++){
-    	for(j = 0; j<T; j++){
-			printf("%f\n",x[trans(i,j)]);
-		}
+	  for(k = 0; k<N-1; k++){
+	    printf("%f\t%f\n",x[k],v[k]);
+	  }
+#pragma omp parallel for \shared(x,v)
+	  for(i = 1; i<N-1; i++){
+	    F_actual = x[i+1] - 2*x[i] + x[i-1] + B*(powf((x[i+1]-x[i]),3) - powf((x[i] - x[i-1]),3));
+	    v_medio = v[i] + (1.0/2)*dt*F_actual;
+	    x_temp[i] = x[i] + dt*v_medio;
+	  }
+	  for(m = 1; m<N-1; m++){
+	    x[m] = x_temp[m];
+	  }
+#pragma omp parallel for \shared(x,v)
+	  for(n = 1; n<N-1; n++){
+	    F_futuro = x[n+1] - 2*x[n] + x[n-1] + B*(powf((x[n+1]-x[n]),3) - powf((x[n] - x[n-1]),3));
+	    v[n] = v_medio + (1.0/2)*dt*F_futuro;
+	  }
 	}
 
-//Imprime los valores de velocidad
-	printf("velocidades \n");
-	for(i = 0; i<N; i++){
-		for(j = 0; j<T; j++){
-     		printf("%f\n",v[trans(i,j)]);
-    	}
-	}
- 
+	free(x);
+	free(v);
 }
 
-//Metodo de solucion de la ecuacion diferencial
-void leapfrog(float *x, float *v, int i,int j, float h , float B){
-	float F_actual = x[trans(i+1,j)] - 2*x[trans(i,j)] + x[trans(i-1,j)] + B*(powf((x[trans(i+1,j)]-x[trans(i,j)]),3) - powf((x[trans(i,j)] - x[trans(i-1,j)]),3));
-    float v_medio = v[trans(i,j)] + (1.0/2)*h*F_actual;
-    x[trans(i,j+1)] = x[trans(i,j)] + h*v_medio;
-    float F_futuro = x[trans(i+1,j)] - 2*x[trans(i,j)] + x[trans(i-1,j)] + B*(powf((x[trans(i+1,j)]-x[trans(i,j)]),3) - powf((x[trans(i,j)] - x[trans(i-1,j)]),3));
-    v[trans(i,j+1)] = v_medio + (1.0/2)*h*F_futuro;
-}  
 
-//Buscador de el lugar en la matriz
-int trans(int i, int j){ 
-	return j*N + i;
-}
+
