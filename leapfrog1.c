1f80f1b2e22e79a246010c5104f8c1d2901cd4ea
DavidAlejandroHenriquez <da.henriquez10@uniandes.edu.co>
2017-03-09 23:22:37 -0500

    6.0

--- leapfrog1.c
+++ leapfrog1.c
@@ -4,7 +4,7 @@
 
 void leapfrog(float *x, float *v, int i,int j, float h , float B);
 
-float dt = 0.005, B = 1.0;
+float dt = 1.0/200, B = 1.0;
 float F_actual,F_futuro,v_medio;
 int N = 64, T = 10000001,i,j,k,n,m;
 
@@ -26,7 +26,8 @@ int main (int argc, char ** argv){
 	}
 
 //solucion de la ecuacion diferencial con el metodo leapfrog
-	float x_temp[N-2];
+	float x_temp[N-1];
+	float v_medio_temp[N-1];
 	for(j = 0; j<T;j++){
 	  if(j%10000 == 0 ){
 	    for(k = 0; k<N; k++){
@@ -35,9 +36,10 @@ int main (int argc, char ** argv){
 	  }
 #pragma omp parallel for \shared(x,v)
 	  for(i = 1; i<N-1; i++){
-	    F_actual = x[i+1] - 2*x[i] + x[i-1] + B*(powf((x[i+1]-x[i]),3) - powf((x[i] - x[i-1]),3));
+	    F_actual = x[i+1] - 2*x[i] + x[i-1] + B*(powf((x[i+1]-x[i]),2) - powf((x[i] - x[i-1]),2));
 	    v_medio = v[i] + (1.0/2)*dt*F_actual;
 	    x_temp[i] = x[i] + dt*v_medio;
+	    v_medio_temp[i] = v_medio;
 	  }
 	  for(m = 1; m<N-1; m++){
 	    x[m] = x_temp[m];
@@ -45,7 +47,7 @@ int main (int argc, char ** argv){
 #pragma omp parallel for \shared(x,v)
 	  for(n = 1; n<N-1; n++){
 	    F_futuro = x[n+1] - 2*x[n] + x[n-1] + B*(powf((x[n+1]-x[n]),3) - powf((x[n] - x[n-1]),3));
-	    v[n] = v_medio + (1.0/2)*dt*F_futuro;
+	    v[n] = v_medio_temp[n] + (1.0/2)*dt*F_futuro;
 	  }
 	}
 
